2e3c0a5212417c6525f906175a565e6692fcaa2e
AGABA ANDREW <agabaandre@gmail.com>
2023-06-16 17:03:33 +0300

    header fix

--- application/modules/staff/views/staff.php
+++ application/modules/staff/views/staff.php
@@ -1,4 +1,7 @@
 <div class="card">
+  <div class="col-md-12" style="float: right;">
+    <button type="button" class="btn   btn-dark btn-sm btn-bordered" data-bs-toggle="modal" data-bs-target="#add_item">+ Add New Staff</button>
+  </div>
   <div class="card-body">
 
     <div class="table-responsive">
@@ -18,16 +21,25 @@
             <th>Email</th>
             <th>Telephone</th>
             <th>WhatsApp</th>
+            <th>Funder</th>
+            <th>Contracting Organisation</th>
+            <th>Grade</th>
+            <th>Contract Type</th>
+            <th>Contract Status</th>
+            <th>Contract Start Date</th>
+            <th>Contract End Date</th>
+            <th>Contract Comments</th>
             <th>Actions</th>
           </tr>
         </thead>
         <tbody>
           <?php
           //dd($staff->toArray());
+          $i = 1;
           foreach ($staff as $data) : ?>
             <tr>
 
-              <td>#</td>
+              <td><?=$i++?></td>
               <td><?= $data->SAPNO ?></td>
               <td><?= $data->title ?></td>
               <td><?= $data->lname . ' ' . $data->fname . ' ' . @$data->oname ?></td>
@@ -40,6 +52,14 @@
               <td><?= @$data->work_email ?></td>
               <td><?= @$data->tel_1 . ' / ' . $data->tel_2 ?></td>
               <td><?= @$data->whatsapp ?></td>
+              <td><?= @$data->contracts[0]->funder_name ?></td>
+              <td><?= @$data->contracts[0]->contractor_name ?></td>
+              <td><?= @$data->contracts[0]->grade_name ?></td>
+              <td><?= @$data->contracts[0]->contract_type_name ?></td>
+              <td><?= @$data->contracts[0]->status ?></td>
+              <td><?= @$data->contracts[0]->start_date ?></td>
+              <td><?= @$data->contracts[0]->end_date ?></td>
+              <td><?= @$data->contracts[0]->comments ?></td>
 
 
               <td><a href="<?= base_url() ?>staff/update">Edit</a> | <a href="<?= base_url() ?>staff/profile">Profie</a></td>
