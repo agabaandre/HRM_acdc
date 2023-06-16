444e1ef43f519a92c3f56f4600e6dcea44c3b6d2
AGABA ANDREW <agabaandre@gmail.com>
2023-06-16 16:32:04 +0300

    header fix

--- application/modules/staff/models/Staff_mdl.php
+++ application/modules/staff/models/Staff_mdl.php
@@ -10,14 +10,17 @@ class Staff_mdl extends CI_Model
         $this->load->model("Contracts");
     }
 
-    public function get_all($search = [])
+    public function get_all()
     {
-        $query = Employee::orderBy("lname", "desc");
+       
+      $query = Employee::orderBy("lname", "desc");
 
-        if (@$search['nationality_id'])
-        $query->where('nationality_id', 1);
+        // if (@$search['nationality_id'])
+        // $query->where('nationality_id', $search->nationality_id);
+        //implement ci-pagination later
+       // $results = $query->with('contracts', 'contracts.funder')->take(20)->skip(20)->get();
+        $results = $query->with('contracts', 'contracts.funder')->get();
 
-        $results = $query->with('contracts', 'contracts.funder')->take(20)->skip(20)->get();
         return $results;
     }
  
