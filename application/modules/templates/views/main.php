ce763315dae283b38d6da3d8e7c3c847617fd380
AGABA ANDREW <agabaandre@gmail.com>
2023-06-18 00:19:21 +0300

    rethemed the project

--- application/modules/templates/views/main.php
+++ application/modules/templates/views/main.php
@@ -1,50 +1,20 @@
 <?php
 require_once('partials/css_files.php');
-//require_once('partials/preloader.php');
 
+require_once('partials/header.php');
+include("partials/nav.php");
+require_once('partials/breadcrumb.php');
 ?>
 
-<body>
-	<!--wrapper-->
-	<div class="wrapper">
-		<?php
-		require_once('partials/header.php');
-		?>
-
-		<div class="nav-container primary-menu">
+<!-- <div class="nav-container primary-menu">
 			<div class="mobile-topbar-header">
 				<div>
-					<img src="<?php echo base_url() ?>assets/images/africacdc_2.jpeg" class="logo-icon" alt="logo icon">
+					<img src="<?php //echo base_url() 
+								?>assets/images/africacdc_2.jpeg" class="logo-icon" alt="logo icon">
 				</div>
-			</div>
-			<?php include("partials/nav.php"); ?>
-		</div>
-		<div class="page-wrapper">
-			<div class="page-content">
-				<div class="row">
+			</div> -->
 
-					<div class="main-content" style="margin-right: 20px; margin-left: 20px; margin-top: 20px;">
-						<div class="container-fluid">
-							<div class="col-md-12">
-								<?php //include'../controllers/add_users.php';?>
-								<div class="card card-default">
-									<div class="card-header">
-										<div class="row">
-											<div class="col-md-4">
-												<h6><b>
-														<?php echo $title ?>
-													</b></h6>
-											</div>
-											<?php $this->load->view($module . "/" . $view); ?>
-										</div>
-									</div>
 
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
+<?php $this->load->view($module . "/" . $view); ?>
 
-		<?php require("partials/footer.php"); ?>
+<?php require("partials/footer.php"); ?>
