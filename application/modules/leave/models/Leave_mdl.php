2745c10135866625e89c836bf03f9362eeca4bd8
AGABA ANDREW <agabaandre@gmail.com>
2023-07-27 02:09:48 +0300

    approval levels

--- application/modules/leave/models/Leave_mdl.php
+++ application/modules/leave/models/Leave_mdl.php
@@ -69,10 +69,11 @@ class Leave_mdl extends CI_Model
         $this->db->from('staff_leave l');
         $this->db->join('staff s', 'l.staff_id = s.staff_id');
         $this->db->join('leave_types lt', 'l.leave_id = lt.leave_id');
-        $this->db->where('s.staff_id!=', $this->session->userdata('user')->staff_id);
+        $this->db->where('s.staff_id=', $this->session->userdata('user')->staff_id);
         $this->db->or_where('l.supervisor_id', $this->session->userdata('user')->staff_id);
         $this->db->or_where('l.supporting_staff', $this->session->userdata('user')->staff_id);
         $this->db->or_where('l.division_head', $this->session->userdata('user')->staff_id);
+        
 
         if ($status) {
             $this->db->where('l.approval_status', $status);
