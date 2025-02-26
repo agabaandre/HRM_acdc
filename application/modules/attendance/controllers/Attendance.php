48be8d3fa549f932cbb522372430fc7ffbccbb6e
Agaba Andre <agabaandre@gmail.com>
2025-02-26 11:47:30 +0300

    made auto approved

--- application/modules/attendance/controllers/Attendance.php
+++ application/modules/attendance/controllers/Attendance.php
@@ -16,5 +16,37 @@ class Attendance extends MX_Controller
 	  $data['staffs'] = $this->staff_mdl->get_active_staff_data();
 	  echo Modules::run("templates/main", $data);
 	}
+	public function employeeTimeLogs($id = FALSE, $print = false, $from = false, $to = false)
+  {
+
+
+    $post = $this->input->post();
+    if ($post) {
+
+
+      $search_data = $this->input->post();
+
+      $data['from'] = $search_data['date_from'];
+      $data['to'] = $search_data['date_to'];
+    } else {
+
+      $data['from'] = date('Y-m-') . '01';
+      $data['to'] = date('Y-m-d');
+      $search_data['date_from'] = $data['from'];
+      $search_data['date_to'] = $data['to'];
+    }
+
+    $dbresult = $this->empModel->getEmployeeTimeLogs(urldecode($id), 10000, 0, $search_data);
+    $data['timelogs'] = $dbresult['timelogs'];
+
+
+    $data['title'] = "Time Logs";
+    //$data['facilities']=Modules::run("facilities/getFacilities");
+    $data['view'] = 'individual_time_logs';
+
+    $data['module'] = "employees";
+    echo Modules::run("templates/main", $data);
+  }
+
 	
 }//end of class
