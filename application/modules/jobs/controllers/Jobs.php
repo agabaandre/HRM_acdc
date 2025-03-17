8543f0eac702fdbb9d06807a419368a395bb9929
Agaba Andre <agabaandre@gmail.com>
2025-03-17 13:25:55 +0300

    send mails

--- application/modules/jobs/controllers/Jobs.php
+++ application/modules/jobs/controllers/Jobs.php
@@ -173,7 +173,8 @@ public function cron_register(){
         if (count($messages) > 0) {
             foreach ($messages as $message) {
                 $body = $message->body;
-                $to ='kibiyed@africacd.org';
+                $to = $message->email_to;
+                // $to ='kibiyed@africacd.org';
                 $subject = $message->subject;
                 $id = $message->id;
                 $next_run = $this->getNextRunDate($message->end_date);
