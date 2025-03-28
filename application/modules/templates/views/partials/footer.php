15f9623b0ce2110cd0b3ff9813ec1d42c5606665
Agaba Andre <agabaandre@gmail.com>
2025-03-28 13:12:50 +0300

    fix footer bootrap

--- application/modules/templates/views/partials/footer.php
+++ application/modules/templates/views/partials/footer.php
@@ -79,10 +79,10 @@
 </div>
 <!--end switcher-->
 
-<!-- <script src="<?php echo base_url() ?>assets/js/bootstrap.bundle.min.js"></script> -->
+<script src="<?php echo base_url() ?>assets/js/bootstrap.bundle.min.js"></script>
   <!-- jQuery UI Library -->
 <!-- Bootstrap 5 with Popper bundled -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-FHgNHNh4rHcmJ8s9jP3J7iYmMOTtMnJ0A2gU2wZSwRPmpZuUMHefPlU+GfNwH3zU" crossorigin="anonymous"></script>
+<!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-FHgNHNh4rHcmJ8s9jP3J7iYmMOTtMnJ0A2gU2wZSwRPmpZuUMHefPlU+GfNwH3zU" crossorigin="anonymous"></script> -->
 
 
 
