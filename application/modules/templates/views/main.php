2bffa6b58f9dff0e9a99a20129f43a4af3cfc6e8
Agaba Andre <agabaandre@gmail.com>
2025-03-29 13:41:53 +0300

    get person trail

--- application/modules/templates/views/main.php
+++ application/modules/templates/views/main.php
@@ -1,10 +1,16 @@
 <?php
 $session = $this->session->userdata('user');
- $permissions = $session->permissions;
+$permissions = $session->permissions;
+$staff_id = $this->session->userdata('user')->staff_id;
+$current_period = str_replace(' ','-',current_period());
+@$ppa_exists = $this->per_mdl->get_staff_plan($staff_id, $current_period);
 require_once('partials/css_files.php');
 require_once('partials/header.php');
 include("partials/nav.php");
 require_once('partials/breadcrumb.php');
 //print_r($session);
+
+
+//dd($ppa_exists);
 $this->load->view($module . "/" . $view); 
 require("partials/footer.php"); 
