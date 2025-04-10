777fc271084b206a73696e904ed130cc47d5df16
Agaba Andre <agabaandre@gmail.com>
2025-04-11 00:18:57 +0300

    update ppa

--- application/modules/performance/views/staff_list.php
+++ application/modules/performance/views/staff_list.php
@@ -13,14 +13,17 @@
     <div class="card-body">
       <div class="table-responsive">
         <table class="table table-hover align-middle table-bordered table-striped mydata" id="staffStatsTable">
-          <thead class="table-light">
-            <tr class="text-center">
+          <thead class="table-light text-center">
+            <tr>
               <th>#</th>
               <th>Name</th>
               <th>SAPNO</th>
               <th>Division</th>
               <th>Contract Type</th>
               <th>Status</th>
+              <?php if ($type === 'with_pdp'): ?>
+                <th>Recommended Trainings</th>
+              <?php endif; ?>
               <th>Action</th>
             </tr>
           </thead>
@@ -34,10 +37,24 @@
                 <td><?= $staff->division_name ?></td>
                 <td><?= $staff->contract_type ?></td>
                 <td><?= $staff->status ?></td>
+                <?php if ($type === 'with_pdp'): ?>
+                  <td>
+                    <?php if (!empty($staff->training_skills)): ?>
+                      <ul class="mb-0">
+                        <?php foreach ($staff->training_skills as $skill): ?>
+                          <li><?= htmlspecialchars($skill, ENT_QUOTES, 'UTF-8') ?>
+                          </li>
+                        <?php endforeach; ?>
+                      </ul>
+                    <?php else: ?>
+                      <span class="text-muted">None</span>
+                    <?php endif; ?>
+                  </td>
+                <?php endif; ?>
                 <td class="text-center">
                   <?php if (!empty($staff->entry_id)): ?>
                     <a href="<?= base_url('performance/view_ppa/' . $staff->entry_id . '/' . $staff->staff_id) ?>"
-                      class="btn btn-sm btn-outline-primary" target="_blank">
+                       class="btn btn-sm btn-outline-primary" target="_blank">
                       <i class="fa fa-eye"></i> View PPA
                     </a>
                   <?php else: ?>
@@ -46,7 +63,6 @@
                 </td>
               </tr>
             <?php endforeach; ?>
-
           </tbody>
         </table>
       </div>
