0987ca9603ff4bc1f89da38d82a8a049c82520c0
Agaba Andre <agabaandre@gmail.com>
2025-05-18 21:43:45 +0300

    team lead on activities

--- bms/resources/views/partials/nav.blade.php
+++ bms/resources/views/partials/nav.blade.php
@@ -1,67 +1,22 @@
+@php
+$workflowMenuItems = [
+    [
+        'route' => 'memos.index',
+        'icon' => 'bx bx-file',
+        'title' => 'Memos'
+    ],
+    [
+        'route' => 'workflows.index',
+        'icon' => 'bx bx-git-branch',
+        'title' => 'Workflows'
+    ],
+    [
+        'route' => 'approvals.index',
+        'icon' => 'bx bx-check-circle',
+        'title' => 'Approvals'
+    ]
+];
 
-
-            <!--Add our workflow management menu items to the navigation-->
-            @php
-            // Add these menu items to the navigation
-            $workflowMenuItems = [
-            [
-            'route' => 'memos.index',
-            'icon' => 'bx bx-file',
-            'title' => 'Memos'
-            ],
-            [
-            'route' => 'workflows.index',
-            'icon' => 'bx bx-git-branch',
-            'title' => 'Workflows'
-            ],
-            [
-            'route' => 'approvals.index',
-            'icon' => 'bx bx-check-circle',
-            'title' => 'Approvals'
-            ],
-            [
-            'route' => 'divisions.index',
-            'icon' => 'bx bx-building',
-            'title' => 'Divisions'
-            ]
-            ];
-            @endphp
-
-            <div class="nav-container primary-menu">
-                <nav class="navbar navbar-expand-xl w-100">
-                    <ul class="navbar-nav justify-content-start flex-grow-1 gap-1">
-                        <!-- Home -->
-                        <li class="nav-item">
-                            <a href="{{ str_replace('bms/', '', url('home/index')) }}"
-                                class="nav-link {{ Request::is('home/index') ? 'active' : '' }}">
-                                <div class="parent-icon"><i class="bx bx-home-circle"></i></div>
-                                <div class="menu-title">Home</div>
-                            </a>
-                        </li>
-
-
-                        <!-- Workflow Management Dropdown -->
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle {{ Request::is('memos*') || Request::is('workflows*') || Request::is('approvals*') || Request::is('divisions*') ? 'active' : '' }}"
-                                href="#" data-bs-toggle="dropdown">
-                                <div class="parent-icon"><i class="bx bx-list-check"></i></div>
-                                <div class="menu-title">Workflow Management</div>
-                            </a>
-                            <ul class="dropdown-menu">
-                                @foreach($workflowMenuItems as $item)
-                                <li>
-                                    <a class="dropdown-item" href="{{ route($item['route']) }}">
-                                        <i class="{{ $item['icon'] }}"></i> {{ $item['title'] }}
-                                    </a>
-                                </li>
-                                @endforeach
-                            </ul>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
-
-            @php
 $settingsMenuItems = [
     [
         'route' => 'fundtypes.index',
@@ -95,20 +50,54 @@ $settingsMenuItems = [
     ]
 ];
 @endphp
-<!-- Settings Dropdown -->
-<li class="nav-item dropdown">
-    <a class="nav-link dropdown-toggle {{ Request::is('fundtypes*') || Request::is('fundcodes*') || Request::is('directorates*') || Request::is('staff*') || Request::is('requesttypes*') ? 'active' : '' }}"
-        href="#" data-bs-toggle="dropdown">
-        <div class="parent-icon"><i class="bx bx-cog"></i></div>
-        <div class="menu-title">Settings</div>
-    </a>
-    <ul class="dropdown-menu">
-        @foreach($settingsMenuItems as $item)
-        <li>
-            <a class="dropdown-item" href="{{ route($item['route']) }}">
-                <i class="{{ $item['icon'] }}"></i> {{ $item['title'] }}
-            </a>
-        </li>
-        @endforeach
-    </ul>
-</li>
+
+<div class="nav-container primary-menu">
+    <nav class="navbar navbar-expand-xl w-100">
+        <ul class="navbar-nav justify-content-start flex-grow-1 gap-1">
+            <!-- Home -->
+            <li class="nav-item">
+                <a href="{{ str_replace('bms/', '', url('home/index')) }}"
+                   class="nav-link {{ Request::is('home/index') ? 'active' : '' }}">
+                    <div class="parent-icon"><i class="bx bx-home-circle"></i></div>
+                    <div class="menu-title">Home</div>
+                </a>
+            </li>
+
+            <!-- Workflow Management Dropdown -->
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle {{ Request::is('memos*') || Request::is('workflows*') || Request::is('approvals*') ? 'active' : '' }}"
+                   href="#" data-bs-toggle="dropdown">
+                    <div class="parent-icon"><i class="bx bx-list-check"></i></div>
+                    <div class="menu-title">Workflow Management</div>
+                </a>
+                <ul class="dropdown-menu">
+                    @foreach($workflowMenuItems as $item)
+                        <li>
+                            <a class="dropdown-item" href="{{ route($item['route']) }}">
+                                <i class="{{ $item['icon'] }}"></i> {{ $item['title'] }}
+                            </a>
+                        </li>
+                    @endforeach
+                </ul>
+            </li>
+
+            <!-- Settings Dropdown -->
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle {{ Request::is('fundtypes*') || Request::is('fundcodes*') || Request::is('divisions*') || Request::is('directorates*') || Request::is('staff*') || Request::is('requesttypes*') ? 'active' : '' }}"
+                   href="#" data-bs-toggle="dropdown">
+                    <div class="parent-icon"><i class="bx bx-cog"></i></div>
+                    <div class="menu-title">Settings</div>
+                </a>
+                <ul class="dropdown-menu">
+                    @foreach($settingsMenuItems as $item)
+                        <li>
+                            <a class="dropdown-item" href="{{ route($item['route']) }}">
+                                <i class="{{ $item['icon'] }}"></i> {{ $item['title'] }}
+                            </a>
+                        </li>
+                    @endforeach
+                </ul>
+            </li>
+        </ul>
+    </nav>
+</div>
