c820b41067505bb4fc78d232d8d82ba1d65c4ab3
Agaba Andre <agabaandre@gmail.com>
2025-06-20 00:49:56 +0300

    fixed unnecessary migrations

--- apm/database/migrations/2024_06_19_000001_create_non_travel_memo_categories_table.php
+++ apm/database/migrations/2024_06_19_000001_create_non_travel_memo_categories_table.php
@@ -8,15 +8,15 @@ return new class extends Migration
 {
     public function up()
     {
-        Schema::create('non_travel_memo_categories', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->timestamps();
-        });
+        // Schema::create('non_travel_memo_categories', function (Blueprint $table) {
+        //     $table->id();
+        //     $table->string('name');
+        //     $table->timestamps();
+        // });
     }
 
     public function down()
     {
-        Schema::dropIfExists('non_travel_memo_categories');
+       // Schema::dropIfExists('non_travel_memo_categories');
     }
 };
