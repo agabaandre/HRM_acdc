4613bbc204f7816861a7645cb1b96534393a5190
Paul Obunga <paulobunga.one@gmail.com>
2025-06-19 06:42:41 +0300

    Remove edit and delete actions from divisions list view

--- apm/resources/views/divisions/index.blade.php
+++ apm/resources/views/divisions/index.blade.php
@@ -67,36 +67,6 @@
                                     <a href="{{ route('divisions.show', $division->id) }}" class="btn btn-sm btn-info" data-bs-toggle="tooltip" title="View">
                                         <i class="bx bx-show"></i>
                                     </a>
-                                    <a href="{{ route('divisions.edit', $division->id) }}" class="btn btn-sm btn-warning" data-bs-toggle="tooltip" title="Edit">
-                                        <i class="bx bx-edit"></i>
-                                    </a>
-                                    <button type="button" class="btn btn-sm btn-danger" data-bs-toggle="modal" data-bs-target="#deleteDivision{{ $division->id }}" data-bs-toggle="tooltip" title="Delete">
-                                        <i class="bx bx-trash"></i>
-                                    </button>
-                                </div>
-
-                                <!-- Delete Modal -->
-                                <div class="modal fade" id="deleteDivision{{ $division->id }}" tabindex="-1" aria-hidden="true">
-                                    <div class="modal-dialog">
-                                        <div class="modal-content">
-                                            <div class="modal-header">
-                                                <h5 class="modal-title">Delete Division</h5>
-                                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                            </div>
-                                            <div class="modal-body">
-                                                <p>Are you sure you want to delete <strong>{{ $division->division_name }}</strong>?</p>
-                                                <p class="text-danger"><small>This action cannot be undone. If this division has associated records, deletion may fail.</small></p>
-                                            </div>
-                                            <div class="modal-footer">
-                                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
-                                                <form action="{{ route('divisions.destroy', $division->id) }}" method="POST" class="d-inline">
-                                                    @csrf
-                                                    @method('DELETE')
-                                                    <button type="submit" class="btn btn-danger">Delete</button>
-                                                </form>
-                                            </div>
-                                        </div>
-                                    </div>
                                 </div>
                             </td>
                         </tr>
