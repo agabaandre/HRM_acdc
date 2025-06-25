b3e8b967e175c8c021e995c61bc212a3e9dcbdb9
Agaba Andre <agabaandre@gmail.com>
2025-06-26 00:43:17 +0300

    partial completeion of midterm review

--- application/modules/performance/views/midterm.php
+++ application/modules/performance/views/midterm.php
@@ -116,7 +116,7 @@ if ($showApprovalBtns != 'show') echo $showApprovalBtns;
 <?php echo form_close(); ?>
 
 <?php if ($showApprovalBtns == 'show' || in_array('83', $permissions)) {
-    $this->load->view('performance/partials/approval_buttons', compact('ppa', 'ppa_settings', 'session', 'approval_trail','midreadonly','midterm_exists'));
+    $this->load->view('performance/partials/approval_buttons', compact('ppa', 'ppa_settings', 'session', 'approval_trail','midreadonly','midterm_exists','permissions'));
 } ?>
 
 <!-- Approval Trail -->
