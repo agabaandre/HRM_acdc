393d9a8248bf7d40c3145d93dd0e186aedcd5238
nkukehenry <henricsanyu@gmail.com>
2025-08-19 01:11:59 +0300

    actioned matrices fix

--- apm/app/Helpers/CustomHelper.php
+++ apm/app/Helpers/CustomHelper.php
@@ -64,12 +64,10 @@ if (!function_exists('user_session')) {
          
             $user = session('user', []);
             $my_appoval =  ApprovalTrail::where('model_id',"=",$matrix->id)
-            ->where('model_type', "="  , '\App\Models\\'.ucfirst(class_basename($matrix)))
+            ->where('model_type', "="  , 'App\Models\\'.ucfirst(class_basename($matrix)))
             ->where('action','approved')
             //->where('approval_order',$matrix->approval_level)
-            ->where('staff_id',$user['staff_id']);
-            dd(getFullSql($my_appoval));
-            //->pluck('id');
+            ->where('staff_id',$user['staff_id'])->pluck('id');
             
 
             return count($my_appoval)>0;
