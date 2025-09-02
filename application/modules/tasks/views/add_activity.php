5f153684f3704189280d97bdee23659cc081d3e4
Agaba Andre <agabaandre@gmail.com>
2025-09-03 00:22:40 +0300

    all weeks data

--- application/modules/tasks/views/add_activity.php
+++ application/modules/tasks/views/add_activity.php
@@ -8,7 +8,7 @@
                 'novalidate' => 'novalidate'
             ]) ?>
 
-
+<!-- Add New Sub-Activities -->
             <div class="modal-header">
                 <h5 class="modal-title" id="addActivitiesModalLabel">Add New Sub-Activities</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
