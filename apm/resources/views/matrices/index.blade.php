d03534d9b1a6b01dd84006adf7d0cca94d3fa8d3
Agaba Andre <agabaandre@gmail.com>
2025-09-07 14:27:52 +0300

    request for services initial state

--- apm/resources/views/matrices/index.blade.php
+++ apm/resources/views/matrices/index.blade.php
@@ -172,18 +172,18 @@
                                                             : $matrix->key_result_area;
                                                     @endphp
                                                     <button class="btn btn-sm btn-outline-info" data-bs-toggle="modal"  
-                                                        data-bs-target="#kraModal{{ $matrix->id }}">
+                                                        data-bs-target="#kraModalMyDiv{{ $matrix->id }}">
                                                         <i class="bx bx-list-check me-1"></i> {{ is_array($kras) ? count($kras) : 0 }}
                                                         Area(s)
                                                     </button>
 
                                                     <!-- Modal -->
-                                                    <div class="modal fade" id="kraModal{{ $matrix->id }}" tabindex="-1"
-                                                        aria-labelledby="kraModalLabel{{ $matrix->id }}" aria-hidden="true">
+                                                    <div class="modal fade" id="kraModalMyDiv{{ $matrix->id }}" tabindex="-1"
+                                                        aria-labelledby="kraModalLabelMyDiv{{ $matrix->id }}" aria-hidden="true">
                                                         <div class="modal-dialog modal-md modal-dialog-centered">
                                                             <div class="modal-content">
                                                                 <div class="modal-header">
-                                                                    <h5 class="modal-title" id="kraModalLabel{{ $matrix->id }}">
+                                                                    <h5 class="modal-title" id="kraModalLabelMyDiv{{ $matrix->id }}">
                                                                         Key Result Areas - {{ $matrix->year }} {{ $matrix->quarter }}
                                                                     </h5>
                                                                     <button type="button" class="btn-close" data-bs-dismiss="modal"
@@ -212,16 +212,16 @@
                                                         $activities = $matrix->activities;
                                                     @endphp
                                                     <button class="btn btn-sm btn-outline-primary" data-bs-toggle="modal"
-                                                        data-bs-target="#activitiesModal{{ $matrix->id }}">
+                                                        data-bs-target="#activitiesModalMyDiv{{ $matrix->id }}">
                                                         <i class="bx bx-list-ul me-1"></i> {{ $activities->count() }} Activity(ies)
                                                     </button>
                                                     <!-- Modal -->
-                                                    <div class="modal fade" id="activitiesModal{{ $matrix->id }}" tabindex="-1"
-                                                        aria-labelledby="activitiesModalLabel{{ $matrix->id }}" aria-hidden="true">
+                                                    <div class="modal fade" id="activitiesModalMyDiv{{ $matrix->id }}" tabindex="-1"
+                                                        aria-labelledby="activitiesModalLabelMyDiv{{ $matrix->id }}" aria-hidden="true">
                                                         <div class="modal-dialog modal-md modal-dialog-centered">
                                                             <div class="modal-content">
                                                                 <div class="modal-header">
-                                                                    <h5 class="modal-title" id="activitiesModalLabel{{ $matrix->id }}">
+                                                                    <h5 class="modal-title" id="activitiesModalLabelMyDiv{{ $matrix->id }}">
                                                                         Activities - {{ $matrix->year }} {{ $matrix->quarter }}
                                                                     </h5>
                                                                     <button type="button" class="btn-close" data-bs-dismiss="modal"
@@ -351,18 +351,18 @@
                                                                 : $matrix->key_result_area;
                                                         @endphp
                                                         <button class="btn btn-sm btn-outline-info" data-bs-toggle="modal"
-                                                            data-bs-target="#kraModal{{ $matrix->id }}">
+                                                            data-bs-target="#kraModalAll{{ $matrix->id }}">
                                                             <i class="bx bx-list-check me-1"></i> {{ is_array($kras) ? count($kras) : 0 }}
                                                             Area(s)
                                                         </button>
 
                                                         <!-- Modal -->
-                                                        <div class="modal fade" id="kraModal{{ $matrix->id }}" tabindex="-1"
-                                                            aria-labelledby="kraModalLabel{{ $matrix->id }}" aria-hidden="true">
+                                                        <div class="modal fade" id="kraModalAll{{ $matrix->id }}" tabindex="-1"
+                                                            aria-labelledby="kraModalLabelAll{{ $matrix->id }}" aria-hidden="true">
                                                             <div class="modal-dialog modal-md modal-dialog-centered">
                                                                 <div class="modal-content">
                                                                     <div class="modal-header">
-                                                                        <h5 class="modal-title" id="kraModalLabel{{ $matrix->id }}">
+                                                                        <h5 class="modal-title" id="kraModalLabelAll{{ $matrix->id }}">
                                                                             Key Result Areas - {{ $matrix->year }} {{ $matrix->quarter }}
                                                                         </h5>
                                                                         <button type="button" class="btn-close" data-bs-dismiss="modal"
@@ -391,16 +391,16 @@
                                                             $activities = $matrix->activities;
                                                         @endphp
                                                         <button class="btn btn-sm btn-outline-primary" data-bs-toggle="modal"
-                                                            data-bs-target="#activitiesModal{{ $matrix->id }}">
+                                                            data-bs-target="#activitiesModalAll{{ $matrix->id }}">
                                                             <i class="bx bx-list-ul me-1"></i> {{ $activities->count() }} Activity(ies)
                                                         </button>
                                                         <!-- Modal -->
-                                                        <div class="modal fade" id="activitiesModal{{ $matrix->id }}" tabindex="-1"
-                                                            aria-labelledby="activitiesModalLabel{{ $matrix->id }}" aria-hidden="true">
+                                                        <div class="modal fade" id="activitiesModalAll{{ $matrix->id }}" tabindex="-1"
+                                                            aria-labelledby="activitiesModalLabelAll{{ $matrix->id }}" aria-hidden="true">
                                                             <div class="modal-dialog modal-md modal-dialog-centered">
                                                                 <div class="modal-content">
                                                                     <div class="modal-header">
-                                                                        <h5 class="modal-title" id="activitiesModalLabel{{ $matrix->id }}">
+                                                                        <h5 class="modal-title" id="activitiesModalLabelAll{{ $matrix->id }}">
                                                                             Activities - {{ $matrix->year }} {{ $matrix->quarter }}
                                                                         </h5>
                                                                         <button type="button" class="btn-close" data-bs-dismiss="modal"
