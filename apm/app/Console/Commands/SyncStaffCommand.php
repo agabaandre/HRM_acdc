87491cd6281aff8314bde5abb5ed760152ff3c0b
Agaba Andre <agabaandre@gmail.com>
2025-09-11 12:42:07 +0300

    api fixed

--- apm/app/Console/Commands/SyncStaffCommand.php
+++ apm/app/Console/Commands/SyncStaffCommand.php
@@ -42,7 +42,7 @@ class SyncStaffCommand extends Command
             }
 
             $response = Http::withBasicAuth($username, $password)
-                ->get('https://cbp.africacdc.org/staff/share/get_current_staff/YWZyY2FjZGNzdGFmZnRyYWNrZXI');
+                ->get('http://localhost/staff/staff/share/get_current_staff/YWZyY2FjZGNzdGFmZnRyYWNrZXIh');
 
             if (!$response->successful()) {
                 throw new Exception('Failed to fetch data from API: ' . $response->status());
