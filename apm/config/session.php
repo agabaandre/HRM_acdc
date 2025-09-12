cec667169e1d938b25cd0892cf67ea5d2f3d6ea5
Agaba Andre <agabaandre@gmail.com>
2025-09-12 08:48:35 +0300

    login update

--- apm/config/session.php
+++ apm/config/session.php
@@ -32,7 +32,7 @@ return [
     |
     */
 
-    'lifetime' => (int) env('SESSION_LIFETIME', 2), // 2 minutes for testing
+    'lifetime' => (int) env('SESSION_LIFETIME', 120),
     'expire_on_close' => false,
 
     // 'expire_on_close' => env('SESSION_EXPIRE_ON_CLOSE', false),
