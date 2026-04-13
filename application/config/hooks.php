58b9aa2623582112aed4d040296d1ef5c57496ba
Agaba Andre <agabaandre@gmail.com>
2026-04-13 18:32:05 +0300

    fixed api docs

--- application/config/hooks.php
+++ application/config/hooks.php
@@ -11,6 +11,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |	https://codeigniter.com/user_guide/general/hooks.html
 |
 */
+$hook['pre_controller'][] = array(
+    'class'    => '',
+    'function' => 'portal_permission_guard',
+    'filename' => 'Portal_permission_guard.php',
+    'filepath' => 'hooks',
+    'params'   => array()
+);
+
 $hook['post_controller_constructor'][] = array(
     'class'    => '',
     'function' => 'log_user_route_access',
