11199a9fe84c3571177b175b761d9b0abc7f35e8
Agaba Andre <agabaandre@gmail.com>
2026-04-29 00:21:09 +0300

    budget updates

--- apm/config/approval_states.php
+++ apm/config/approval_states.php
@@ -1,8 +1,10 @@
 <?php
 
 return [
-    'pending' =>'bg-warning text-white',
-    'approved'=>'bg-success text-white',
-    'returned'=>'bg-danger text-white',
-    'draft'=>'bg-dark text-white'
+    'pending' => 'bg-warning text-white',
+    'approved' => 'bg-success text-white',
+    'returned' => 'bg-danger text-white',
+    'draft' => 'bg-dark text-white',
+    'rejected' => 'bg-danger text-white',
+    'archived' => 'bg-secondary text-white',
 ];
