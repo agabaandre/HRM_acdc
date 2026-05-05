b766dcdd833c6bf761271171da7b0552b5f5d0cd
Agaba Andre <agabaandre@gmail.com>
2026-05-05 12:15:27 +0300

    docker update

--- apm/resources/views/special-memo/pending-approvals.blade.php
+++ apm/resources/views/special-memo/pending-approvals.blade.php
@@ -211,7 +211,7 @@
                                                             <i class="bx bx-show me-1"></i>View
                                                         </a>
                                                         @if($memo->overall_status === 'draft' && $memo->staff_id === user_session('staff_id'))
-                                                            <a wire:navigate href="{{ route('special-memo.edit', $memo) }}" 
+                                                            <a href="{{ route('special-memo.edit', $memo) }}" 
                                                                class="btn btn-sm btn-outline-warning" title="Edit">
                                                                 <i class="bx bx-edit me-1"></i>Edit
                                                             </a>
@@ -341,7 +341,7 @@
                                                             <i class="bx bx-show me-1"></i>View
                                                         </a>
                                                         @if($memo->overall_status === 'draft' && $memo->staff_id === user_session('staff_id'))
-                                                            <a wire:navigate href="{{ route('special-memo.edit', $memo) }}" 
+                                                            <a href="{{ route('special-memo.edit', $memo) }}" 
                                                                class="btn btn-sm btn-outline-warning" title="Edit">
                                                                 <i class="bx bx-edit me-1"></i>Edit
                                                             </a>
