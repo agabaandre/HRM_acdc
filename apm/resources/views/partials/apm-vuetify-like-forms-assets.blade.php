abc8ac00eed576d9b4b4a5a643cebedec874c165
Agaba Andre <agabaandre@gmail.com>
2026-07-05 09:56:19 +0300

    fix settings divsions display vuetify

--- apm/resources/views/partials/apm-vuetify-like-forms-assets.blade.php
+++ apm/resources/views/partials/apm-vuetify-like-forms-assets.blade.php
@@ -1,5 +1,5 @@
 @once
 @push('head-meta')
-<link rel="stylesheet" href="{{ asset('css/apm-vuetify-like-forms.css') }}?v=3">
+<link rel="stylesheet" href="{{ asset('css/apm-vuetify-like-forms.css') }}?v=4">
 @endpush
 @endonce
