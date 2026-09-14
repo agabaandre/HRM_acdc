9ad904b3d59ed77577884cc6390f596c4655a44f
Agaba Andre <agabaandre@gmail.com>
2026-09-14 23:45:24 +0300

    Add CBP Compose services for Redis, workers, and bundled MySQL.

--- docker/mysql/init/01-databases.sh
+++ docker/mysql/init/01-databases.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 set -e
 
-# Single shared schema for all CBP modules (Staff CI, staff-portal, APM, Finance, Helpdesk).
 STAFF_DB="${STAFF_DB_NAME:-staff}"
+APM_DB="${APM_DB_NAME:-apm_local}"
 
 mysql -uroot -p"${MYSQL_ROOT_PASSWORD}" <<-EOSQL
 CREATE DATABASE IF NOT EXISTS \`${STAFF_DB}\` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE IF NOT EXISTS \`${APM_DB}\` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 GRANT ALL PRIVILEGES ON \`${STAFF_DB}\`.* TO '${MYSQL_USER}'@'%';
+GRANT ALL PRIVILEGES ON \`${APM_DB}\`.* TO '${MYSQL_USER}'@'%';
 FLUSH PRIVILEGES;
 EOSQL
