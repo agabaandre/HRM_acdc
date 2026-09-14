327a455988ad4e2814d920fec4bfbc564841410e
Agaba Andre <agabaandre@gmail.com>
2026-09-14 21:33:27 +0300

    Remove leftover legacy root clutter after the CodeIgniter cutover.

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
 /**
  * Staff web root front controller.
- * The CodeIgniter staff app has been removed; the Vue/Laravel staff-portal
  * is served from this repository root via .htaccess → staff-portal/.
  */
 declare(strict_types=1);
--- materialpro-nuxtjs-free-master 2/package/components/layout/full/logo/Logo.vue
+++
@@ -1,8 +0,0 @@
-<script setup>
-import Logo from "/images/logos/logo.svg";
-</script>
-<template>
-  <NuxtLink to="/" class="lh-0">
-    <img :src="Logo" alt="logo" />
-  </NuxtLink>
-</template>
